330065d26f2241622ebf5b68290aeedf88af8bb0
Niki-Timofe <niki-timofe@ya.ru>
2016-10-27 17:43:27 +0300

    Initial Commit

---
+++ NarodMonWidget/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NarodMonWidget
+//
+//  Created by Никита Тимофеев on 27.10.16.
+//  Copyright © 2016 Никита Тимофеев. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
