0fe0b9e499ffebc37ed08889309e5758ab57a9ab
Никита Тимофеев <nikita@nikita-mac.local>
2016-10-28 15:42:21 +0300

    Seems to be done with API

--- NarodMonWidget/StatusMenuController.swift
+++ NarodMonWidget/StatusMenuController.swift
@@ -13,24 +13,35 @@ class StatusMenuController: NSObject, NarodMonAPIDelegate {
     
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
     var narodMonAPI: NarodMonAPI!
+    var sensorsList: [UInt:Type] = [:]
 
     override func awakeFromNib() {
         narodMonAPI = NarodMonAPI(delegate: self)
-        statusItem.title = "~º"
+        statusItem.title = "~"
         statusItem.menu = statusMenu
+    }
+    
+    func gotSensorsList(sensors: [Sensor]) {
+        var sensList: [UInt] = []
+        
+        for sensor in sensors {
+            sensorsList.updateValue(sensor.type, forKey: sensor.id)
+            sensList.append(sensor.id)
+        }
+        narodMonAPI.sensorsValues(sensors: sensList)
     }
     
     func gotSensorsValues(rdgs: [Reading]) {
         var inTitle = ""
         for reading in rdgs {
-            inTitle += String(format: "%f%@ ", reading.value, reading.type.unit)
+            inTitle += String(format: "%.1f%@ ", reading.value, sensorsList[reading.sensor]!.unit)
         }
         inTitle = inTitle.substring(to: inTitle.index(before: inTitle.endIndex))
         statusItem.title = inTitle
     }
     
     func appInitiated(app: App) {
-        if app.latest < Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String {
+        if app.latest > Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String {
             let alert = NSAlert()
             alert.addButton(withTitle: "Сейчас")
             alert.addButton(withTitle: "Потом")
@@ -44,6 +55,8 @@ class StatusMenuController: NSObject, NarodMonAPIDelegate {
                 }
             }
         }
+        
+        narodMonAPI.sensorsNearby()
     }
     
     @IBAction func quitBtnAction(_ sender: NSMenuItem) {
