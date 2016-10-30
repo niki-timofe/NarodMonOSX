e60f9a4a25a73c367b819d8f9b9200eeb98ff4f7
Niki-Timofe <niki-timofe@ya.ru>
2016-10-30 16:48:35 +0300

    Better menu view

---
+++ NarodMonWidget/WeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherView.swift
+//  NarodMonWidget
+//
+//  Created by Никита Тимофеев on 29.10.16.
+//  Copyright © 2016 Никита Тимофеев. All rights reserved.
+//
+
+import Cocoa
+
+class WeatherView: NSView {
+    @IBOutlet weak var infoTextField: NSTextField!
+    @IBOutlet weak var conditionsTextField: NSTextField!
+    
+//    func update(weather: Weather) {
+//        DispatchQueue.main.async {
+//            
+//        }
+//    }
+}
