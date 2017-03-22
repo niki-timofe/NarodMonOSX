eb38b7481bb890ba254bb53409bf989dbf74fdc2
Niki-Timofe <niki-timofe@ya.ru>
2017-03-22 18:23:14 +0300

    Initial Commit

---
+++ NarodMonMenubarApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NarodMonMenubarApp
+//
+//  Created by Никита Тимофеев on 22.03.17.
+//  Copyright © 2017 Nikita Timofeev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
